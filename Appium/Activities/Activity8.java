abd247fa6ad20c484fa6c1791200a6db13b3da54
akshathamgowda <41184265+akshathamgowda@users.noreply.github.com>
2024-09-18 17:26:50 +0530

    Add files via upload

---
+++ Appium/Activities/Activity8.java
@@ -0,0 +1,78 @@
+package Activity;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+
+public class Activity8 {
+    WebDriverWait wait;
+    AndroidDriver driver;
+
+    @BeforeClass
+    public void beforeClass() throws MalformedURLException {
+        // Desired Capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("Android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.android.chrome");
+        options.setAppActivity("com.google.android.apps.chrome.Main");
+        options.noReset();
+
+        // Server URL
+        URL serverURL = new URL("http://localhost:4723/wd/hub");
+
+        // Driver initialization
+        driver = new AndroidDriver(serverURL, options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        
+        // Open Selenium page
+        driver.get("https://www.training-support.net/selenium");
+    }
+
+    @Test
+    public void scrollIntoViewTest() {
+        String UiScrollable = "UiScrollable(UiSelector().scrollable(true))";
+        // Wait for the page to load
+        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.view.View")));
+
+        // Just scroll
+        driver.findElement(AppiumBy.androidUIAutomator(UiScrollable + ".flingForward()"));
+        // Locate element
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Tab opener']")).click();
+
+        // Scroll and locate elements
+        driver.findElement(AppiumBy.androidUIAutomator(UiScrollable + ".scrollTextIntoView(\"Tab Opener\")")).click();
+
+        // Wait for the page to load
+        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("Click Me!")));
+        // Print page title
+        String pageTitle = driver.findElement(AppiumBy.xpath("//android.widget.TextView[contains(@text, 'New Tab')]")).getText();
+        System.out.println("Page title is: " + pageTitle);
+
+        // Assertion
+        Assert.assertEquals(pageTitle, "New Tab Opener");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
+
+
